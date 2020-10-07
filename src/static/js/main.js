f17359864adad96407f392a13c618eb2bfd65ed7
Vladislav Bykov <therockshade@gmail.com>
2020-10-07 13:26:02 +0400

    feat: add popup windows

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,6 +1,7 @@
+let body = document.querySelector("body");
+
 (function () {
-  let body = document.querySelector("body"),
-    menu = document.querySelector(".menu"),
+  let menu = document.querySelector(".menu"),
     menuOpen = document.querySelector(".menu__open"),
     menuClose = document.querySelector(".menu__close"),
     focusItem = document.querySelector(".js_focus");
@@ -19,7 +20,68 @@
 
   window.addEventListener("keydown", (e) => {
     if (e.code === "Escape" && menu.classList.contains("js_open")) {
-      menu.classList.remove("js_open");
+			menu.classList.remove("js_open");
+			body.classList.remove("js_overflow");
+			menuOpen.focus();
     }
   });
+})();
+
+(function () {
+  let loginOpen = document.querySelector(".js_signin"),
+		loginPopup = document.querySelector(".popup--login");
+		
+  let loginOpenMenu = document.querySelector(".js_signin--menu"),
+    loginPopupMenu = document.querySelector(".popup--login");
+
+  let registerOpen = document.querySelector(".js_register"),
+		registerPopup = document.querySelector(".popup--register");
+		
+  let registerOpenMenu = document.querySelector(".js_register--menu"),
+    registerPopupMenu = document.querySelector(".popup--register");
+
+  let messageOpen = document.querySelector(".js_message"),
+    messagePopup = document.querySelector(".popup--message");
+
+  let changePasswordOpen = document.querySelector(".js_change-password"),
+    changePasswordPopup = document.querySelector(".popup--change-password");
+
+  let changeDataOpen = document.querySelector(".js_change-data"),
+    changeDataPopup = document.querySelector(".popup--change-data");
+
+  function popup(button, popup) {
+		if (button && popup) {
+			let close = popup.querySelector(".popup__close");
+
+			button.addEventListener("click", () => {
+				let focus = popup.querySelector(".popup__input");
+	
+				popup.classList.add("js_open");
+				body.classList.add("js_overflow");
+				focus.focus();
+			});
+	
+			close.addEventListener("click", () => {
+				popup.classList.remove("js_open");
+				body.classList.remove("js_overflow");
+				button.focus();
+			});
+	
+			window.addEventListener("keydown", (e) => {
+				if (e.code === "Escape" && popup.classList.contains("js_open")) {
+					popup.classList.remove("js_open");
+					body.classList.remove("js_overflow");
+					button.focus();
+				}
+			});
+		}
+  }
+
+  popup(loginOpen, loginPopup);
+  popup(loginOpenMenu, loginPopupMenu);
+  popup(registerOpen, registerPopup);
+  popup(registerOpenMenu, registerPopupMenu);
+  popup(messageOpen, messagePopup);
+  popup(changePasswordOpen, changePasswordPopup);
+  popup(changeDataOpen, changeDataPopup);
 })();
