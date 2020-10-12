efc3a5caed23b071d85ae8f0564b1dd4f28929a3
Vladislav Bykov <therockshade@gmail.com>
2020-10-12 14:14:21 +0400

    feat: add swiper slider

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -4,3 +4,12 @@
   };
   slider(option);
 })();
+
+var mySwiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
