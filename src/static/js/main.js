d7d63f864a637ed832c1c2289383b14d2c27af3d
Vladislav Bykov <therockshade@gmail.com>
2020-12-03 14:23:28 +0400

    fix: fix message popup

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -193,6 +193,93 @@ const SERVER_URL = "https://academy.directlinedev.com",
 	}
 })();
 
+/* --- Message Popup --- */
+
+(() => {
+	const open = document.querySelector(".message_js"),
+				window = document.querySelector(".popup--message"),
+				form = document.forms["message"];
+
+	let isLoading = false;
+
+	if (open) {
+		open.addEventListener("click", () => {
+			popup(window, open, form);
+		})
+	}
+
+	form.addEventListener("submit", (e) => {
+		submit(e);
+	})
+
+	function submit(e) {
+		e.preventDefault();
+
+		if (isLoading) {
+			return;
+		}
+		isLoading = true;
+
+		const body = getFormData(e.target);
+		let errors = validateData(body);
+		
+		if (Object.keys(errors).length > 0) {
+			setFormErrors(e.target, errors);
+			isLoading = false;
+		} else {
+			let newData = {
+				to: body.to,
+				body: JSON.stringify(body)
+			}
+			fetchData({
+				method: "POST",
+				url: "/api/emails",
+				body: JSON.stringify(newData),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+			.then(res => { return res.json(); })
+			.then(res => {
+				if (res.success) {
+					setFormSuccess(e.target);
+					setTimeout(() => {
+						popupClose(window, open, form);
+						answer(answerPopup, "Сообщение было успешно отправлено", "success");
+					}, 2000);
+				} else {
+					throw res;
+				}
+				isLoading = false;
+			})
+			.catch(err => {
+				if (err.errors) {
+					setFormErrors(e.target, err.errors);
+				} else {
+					answer(answerPopup, "Ошибка сервера", "error");
+				}
+				isLoading = false;
+			})
+		}
+	}
+
+	function validateData(data, errors = {}) {
+		if (data.name === "") {
+			errors.name = "Пожалуйста, введите своё имя";
+		}
+		if (data.subject === "") {
+			errors.subject = "Пожалуйста, введите тему сообщения";
+		}
+		if (data.to === "") {
+			errors.to = "Пожалуйста, введите ваш email";
+		}
+		if (!checkTelephone(data.telephone)) {
+			errors.telephone = "Пожалуйста, введите валидный номер телефона";
+		}
+		return errors;
+	}
+})();
+
 /* --- Mobile Menu --- */
 
 (() => {
